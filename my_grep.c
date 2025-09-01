f134d75e503fc124bd98e83d2547e18864c4c568
tarnaldo88 <tarnaldo88@gmail.com>
2025-08-31 23:46:37 -0700

    implemented seach pattern function

--- my_grep.c
+++ my_grep.c
@@ -4,6 +4,21 @@
 #include <string>
 #include <regex>
 
+void search_pattern(FILE *file, const char* pattern)
+{
+    char line[1024];
+    int line_number = 1 ;
+
+    while(fgets(line, sizeof(line), file))
+    {
+        if(strstr(line, pattern))
+        {
+            printf("%d: %s", line_number, line);
+        }
+        line_number++;
+    }
+}
+
 int main(int argc, char* argv[])
 {
     if(argc < 3)
@@ -14,4 +29,13 @@ int main(int argc, char* argv[])
 
     FILE * file = fopen(argv[2], "r");
 
+    if(!file)
+    {
+        printf("Error opening file\n");
+        return 1;
+    }
+
+    printf("Simple grep (non-regex):\n");
+
+    search_pattern(file, argv[1]);
 }
